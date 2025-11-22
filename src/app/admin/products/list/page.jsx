ed71018fe73d468182f89bc1fbef780a96dba01e
abusayedsohag <abusayedsohagislam25@gmail.com>
2025-11-23 01:09:53 +0600

    products list page update

--- src/app/admin/products/list/page.jsx
+++ src/app/admin/products/list/page.jsx
@@ -16,23 +16,21 @@ const Products = () => {
 
     return (
         <div>
-            <div className='flex justify-between'>
-                <h1 className='text-2xl font-semibold'>Products List</h1>
+            <div className='flex justify-between items-center'>
+                <h1 className='md:text-2xl font-semibold'>Products List</h1>
                 <div className='flex gap-2'>
-                    {/* filter btn */}
                     <div className="dropdown">
-
-                        <div tabIndex={0} role="button" className="btn m-1">
+                        <div tabIndex={0} role="button" className="btn m-1 btn-dash btn-xs md:btn-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-filter"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" /></svg>
                             Filter</div>
-                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-40 p-2 shadow-sm">
                             <li><a>A-to-Z</a></li>
                             <li><a>Low-to-High</a></li>
                             <li><a>High-to-Low</a></li>
                         </ul>
                     </div>
 
-                    <a href='/admin/products/add' tabIndex={0} role="button" className="btn m-1 btn-primary">
+                    <a href='/admin/products/add' tabIndex={0} role="button" className="btn m-1 btn-xs md:btn-sm btn-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="inline-block size-4 my-1.5"><rect x="3" y="3" width="18" height="18" rx="2" ry="2" /><line x1="12" y1="8" x2="12" y2="16" /><line x1="8" y1="12" x2="16" y2="12" /></svg>
                         Add
                     </a>
@@ -82,16 +80,18 @@ const Products = () => {
                                         </td>
                                         <td>{info.pStock}</td>
                                         <td>{info.discount} %</td>
-                                        <td><p className='badge badge-dash'>{info.pstatus}</p></td>
+                                        <td><p className='badge badge-xs badge-dash'>{info.pstatus}</p></td>
 
-                                        <th className='flex items-center'>
-                                            <a href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
-                                            </a>
-                                            <a href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/></svg>
-                                            </a>
-                                        </th>
+                                        <td className=''>
+                                            <div className='flex justify-between items-center'>
+                                                <a href="#">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" /><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" /></svg>
+                                                </a>
+                                                <a href="#">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /></svg>
+                                                </a>
+                                            </div>
+                                        </td>
 
                                     </tr>
                                 ))
