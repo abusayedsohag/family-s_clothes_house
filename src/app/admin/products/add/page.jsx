e4e9032450afb1fd4bf3da7eb6187f794e0ba260
abusayedsohag <abusayedsohagislam25@gmail.com>
2025-11-24 00:32:06 +0600

    products add page update

--- src/app/admin/products/add/page.jsx
+++ src/app/admin/products/add/page.jsx
@@ -226,8 +226,10 @@ const AddPro = () => {
                     {images.map((img, index) => (
                         <div key={index} className={`cursor-pointer relative group flex items-center justify-center border aspect-square rounded border-neutral-300 bg-base-300 ${index === 0 ? "col-span-2 row-span-2" : ""
                             }`} >
-                            {img ? (
-                                <>
+                            {img ? (loading === index ? (
+                                <Spinner></Spinner>
+                            ) :
+                                (<>
                                     <img
                                         src={img}
                                         alt={`img-${index}`}
@@ -244,7 +246,6 @@ const AddPro = () => {
                                                 accept="image/*"
                                                 name={`image${index}`}
                                                 className="sr-only"
-                                                required={index === 0}
                                                 onChange={(e) => handleImgUpload(index, e)}
                                             />
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" /><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" /></svg>
@@ -258,7 +259,7 @@ const AddPro = () => {
                                         </button>
 
                                     </div>
-                                </>
+                                </>)
                             ) : (
                                 loading === index ? (
                                     <Spinner></Spinner>
